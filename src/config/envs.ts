6c5e3e42540ec5ae4dec4dcd855d8ee591fc2d31
Edgar Vergara <edgarvm13@hotmail.com>
2025-02-28 14:53:06 +0100

    feat: add dotenv and joi for environment variable management and validation

---
+++ src/config/envs.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+  })
+  .unknown(true);
+
+const result = envsSchema.validate(process.env);
+const error: joi.ValidationError | undefined = result.error;
+const value: EnvVars | undefined = result.value as EnvVars;
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Orders');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(envs.port);
+
+  logger.log(`Orders microservice running on port ${envs.port}`);
 }
-bootstrap();
+void bootstrap();
