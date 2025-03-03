1fa8fe72d9f71209143116d256574d3ee81f354a
Edgar Vergara <edgarvm13@hotmail.com>
2025-03-03 09:50:02 +0100

    feat: implement orders microservice with create, find, and status change functionalities

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Orders');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
 
-  await app.listen(envs.port);
+  await app.listen();
 
   logger.log(`Orders microservice running on port ${envs.port}`);
 }
